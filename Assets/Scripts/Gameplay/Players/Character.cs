724271559248e3faf738a1b970e00a8d18f09f43
aNnAm2606 <73582949+aNnAm2606@users.noreply.github.com>
2023-10-20 12:40:56 +0200

    starting the game

---
+++ Assets/Scripts/Gameplay/Players/Character.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Character : MonoBehaviour
+{
+    private Controller userInput;
+
+
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        
+    }
+
+   
+}
