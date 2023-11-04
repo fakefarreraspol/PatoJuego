38c5eeba93e342d809505cd34976b9fc66cc2e40
Pol <79444349+fakefarreraspol@users.noreply.github.com>
2023-11-04 21:41:39 +0100

    working on the game

--- Assets/Scripts/Gameplay/DefaultController.cs
+++ Assets/Scripts/Gameplay/DefaultController.cs
@@ -35,11 +35,20 @@ public partial class @Controller : IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": true
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""1db4639b-a4e9-43c9-a116-32666162309a"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
                 {
-                    ""name"": ""WASD"",
+                    ""name"": ""AD"",
                     ""id"": ""35184a15-101e-4330-8969-77b7fc5db05d"",
                     ""path"": ""2DVector"",
                     ""interactions"": """",
@@ -48,28 +57,6 @@ public partial class @Controller : IInputActionCollection2, IDisposable
                     ""action"": ""Move"",
                     ""isComposite"": true,
                     ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": ""up"",
-                    ""id"": ""580d36ec-06d0-4143-8864-df99417d771e"",
-                    ""path"": ""<Keyboard>/w"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""down"",
-                    ""id"": ""3cf5a0e6-413a-4598-8b39-c1b29d3c3f84"",
-                    ""path"": ""<Keyboard>/s"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
                 },
                 {
                     ""name"": ""left"",
@@ -92,6 +79,17 @@ public partial class @Controller : IInputActionCollection2, IDisposable
                     ""action"": ""Move"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""0a2d5714-d109-48fa-970d-c9b821122f0a"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -101,6 +99,7 @@ public partial class @Controller : IInputActionCollection2, IDisposable
         // Player
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -161,11 +160,13 @@ public partial class @Controller : IInputActionCollection2, IDisposable
     private readonly InputActionMap m_Player;
     private IPlayerActions m_PlayerActionsCallbackInterface;
     private readonly InputAction m_Player_Move;
+    private readonly InputAction m_Player_Jump;
     public struct PlayerActions
     {
         private @Controller m_Wrapper;
         public PlayerActions(@Controller wrapper) { m_Wrapper = wrapper; }
         public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -178,6 +179,9 @@ public partial class @Controller : IInputActionCollection2, IDisposable
                 @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
                 @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
                 @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
             }
             m_Wrapper.m_PlayerActionsCallbackInterface = instance;
             if (instance != null)
@@ -185,6 +189,9 @@ public partial class @Controller : IInputActionCollection2, IDisposable
                 @Move.started += instance.OnMove;
                 @Move.performed += instance.OnMove;
                 @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
             }
         }
     }
@@ -192,5 +199,6 @@ public partial class @Controller : IInputActionCollection2, IDisposable
     public interface IPlayerActions
     {
         void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
     }
 }
--- Assets/Scripts/Gameplay/Players/Character.cs
+++ Assets/Scripts/Gameplay/Players/Character.cs
@@ -7,19 +7,76 @@ public class Character : MonoBehaviour
 {
     private Controller userInput;
 
+    [SerializeField] private int health;
+    private float speed = 20;
+    private int damage;
+    private float rateOfFire;
+    private float cooldown;
 
-
-    // Start is called before the first frame update
-    protected void Start()
-    {
-        
-    }
-
+    protected bool isPlayerAttacking = false;
+    protected bool canPlayerAttack = true;
     // Update is called once per frame
-    protected void Update()
+    protected Vector2 attackVector = Vector2.zero;
+
+
+
+    private Rigidbody2D characterRb;
+    private Vector2 moveVector = Vector2.zero;
+
+    private void Awake()
     {
+        characterRb = GetComponent<Rigidbody2D>();
+        userInput = new Controller();
+    }
+    private void OnEnable()
+    {
+        userInput.Enable();
+
+        userInput.Player.Move.performed += OnMovementPerformed;
+        userInput.Player.Move.canceled += OnMovementStopped;
+
+        userInput.Player.Jump.performed += OnJumpPerformed;
+        userInput.Player.Jump.performed += OnJumpCancelled;
+    }
+    private void OnDisable()
+    {
+        userInput.Disable();
         
+        userInput.Player.Move.performed -= OnMovementPerformed;
+        userInput.Player.Move.canceled -= OnMovementStopped;
+
+        userInput.Player.Jump.performed -= OnJumpPerformed;
+        userInput.Player.Jump.performed -= OnJumpCancelled;
+    }
+    
+    
+    // Update is called once per frame
+    protected void FixedUpdate()
+    {
+        Vector2 velocity = new Vector2(moveVector.x * speed, 0);
+        characterRb.velocity = velocity;
     }
 
-   
+    private void OnMovementPerformed(InputAction.CallbackContext value)
+    {
+        moveVector = value.ReadValue<Vector2>();
+        Debug.Log(moveVector);
+        //PlayerAnimations.OnSpriteChanged(moveVector);
+    }
+
+    private void OnMovementStopped(InputAction.CallbackContext value)
+    {
+        moveVector = Vector2.zero;
+        //PlayerAnimations.OnSpriteChanged(moveVector);
+    }
+
+    private void OnJumpPerformed(InputAction.CallbackContext value)
+    {
+        Debug.Log(value.ToString());
+    }
+
+    private void OnJumpCancelled(InputAction.CallbackContext value)
+    {
+        Debug.Log(value.ToString());
+    }
 }
