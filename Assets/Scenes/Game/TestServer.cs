f695fb584fd079c5ffe70c4c39c368a672b4576f
Pol <79444349+fakefarreraspol@users.noreply.github.com>
2023-11-18 18:29:28 +0100

    client gets disconnections and handles multiple clients

--- Assets/Scenes/Game/TestServer.cs
+++ Assets/Scenes/Game/TestServer.cs
@@ -6,15 +6,15 @@ using System.Net;
 using System.Text;
 using TMPro;
 using UnityEngine;
+using System.Linq;
 
 public class TestServer : MonoBehaviour
 {
     private UdpClient udpServer;
     private int port = 8080;
-
-
-    private Vector3 rValue;
-    [SerializeField] private GameObject malomalisimo;
+    private Dictionary<IPEndPoint, string> connectedClients = new Dictionary<IPEndPoint, string>();
+    private Dictionary<IPEndPoint, DateTime> lastReceivedTime = new Dictionary<IPEndPoint, DateTime>();
+    private TimeSpan timeoutThreshold = TimeSpan.FromSeconds(5); // Adjust as needed
 
     void Start()
     {
@@ -32,35 +32,90 @@ public class TestServer : MonoBehaviour
 
     private void ReceiveCallback(IAsyncResult result)
     {
+        IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
         try
         {
-            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
             byte[] data = udpServer.EndReceive(result, ref remoteEndPoint);
+
+            // Check if data is null, which may indicate a disconnection
+            if (data == null)
+            {
+                HandleClientDisconnect(remoteEndPoint);
+                return;
+            }
 
             if (data.Length > 0)
             {
                 string message = Encoding.UTF8.GetString(data);
-                Debug.Log("Received from " + remoteEndPoint + ": " + message);
+                Debug.Log("Received from " + remoteEndPoint + " (" + GetClientName(remoteEndPoint) + "): " + message);
 
-                float w= float.Parse(message);
-                rValue = new Vector3(w, 0, 0);
-
-                Debug.Log("rrr, " + rValue.ToString());
+                // Update last received time for the client
+                lastReceivedTime[remoteEndPoint] = DateTime.Now;
             }
 
             // Continue listening for more data
             udpServer.BeginReceive(ReceiveCallback, null);
         }
+        catch (ObjectDisposedException)
+        {
+            // The UdpClient is disposed, indicating that the server is closing
+            Debug.Log("Server is shutting down.");
+        }
         catch (Exception e)
         {
-            Debug.LogError("Error receiving data: " + e.Message);
+            if (e is SocketException && ((SocketException)e).SocketErrorCode == SocketError.ConnectionReset)
+            {
+                // Connection reset by the client, indicating disconnection
+                HandleClientDisconnect(remoteEndPoint);
+            }
+            else
+            {
+                Debug.LogError("Error receiving data: " + e.Message);
+            }
         }
     }
 
-    private void FixedUpdate()
+    private void HandleClientDisconnect(IPEndPoint clientEndPoint)
     {
+        Debug.Log("Client " + GetClientName(clientEndPoint) + " has disconnected.");
+        connectedClients.Remove(clientEndPoint);
+        lastReceivedTime.Remove(clientEndPoint);
+    }
 
-        malomalisimo.transform.position = new Vector3(rValue.x, malomalisimo.transform.position.y, malomalisimo.transform.position.z);
+    private string GetClientName(IPEndPoint clientEndPoint)
+    {
+        if (connectedClients.ContainsKey(clientEndPoint))
+        {
+            return connectedClients[clientEndPoint];
+        }
+        else
+        {
+            string clientName = "Client" + connectedClients.Count;
+            connectedClients.Add(clientEndPoint, clientName);
+            lastReceivedTime.Add(clientEndPoint, DateTime.Now);
+            return clientName;
+        }
+    }
+
+    void Update()
+    {
+        // Check for client timeouts
+        CheckClientTimeouts();
+    }
+
+    private void CheckClientTimeouts()
+    {
+        DateTime currentTime = DateTime.Now;
+
+        foreach (var kvp in lastReceivedTime.ToList())
+        {
+            if (currentTime - kvp.Value > timeoutThreshold)
+            {
+                // The client has not sent data for a while, consider it disconnected
+                HandleClientDisconnect(kvp.Key);
+            }
+        }
     }
 
     void OnApplicationQuit()
--- Assets/Scripts/Gameplay/Players/PlayerAnimations.cs
+++ Assets/Scripts/Gameplay/Players/PlayerAnimations.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
 using System;
-using UnityEditor.Experimental.GraphView;
 
 public class PlayerAnimations : MonoBehaviour
 {
