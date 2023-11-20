06e27758e5686554154ab097efaca54fa5a06604
aNnAm2606 <73582949+aNnAm2606@users.noreply.github.com>
2023-11-20 09:04:41 +0100

    Fixed Jump

--- Assets/Scripts/Gameplay/Players/Character.cs
+++ Assets/Scripts/Gameplay/Players/Character.cs
@@ -6,37 +6,24 @@ using UnityEngine.InputSystem;
 public class Character : MonoBehaviour
 {
     private Controller userInput;
+    private Rigidbody2D characterRb;
 
     [SerializeField] private int health;
     private float speed = 20;
-    private int damage;
-    private float rateOfFire;
-    private float cooldown;
+    [SerializeField] private float jumpForce = 3000.0f;
 
-    protected bool isPlayerAttacking = false;
-    protected bool canPlayerAttack = true;
-    // Update is called once per frame
-    protected Vector2 attackVector = Vector2.zero;
-
-
-
-    private Rigidbody2D characterRb;
     private Vector2 moveVector = Vector2.zero;
-
-
 
     // Variables
     private bool canJump = true;
-    private bool isJumping = false;
     private bool jumped = false;
-    [SerializeField] protected float jumpForce = 1000.0f; 
-
 
     private void Awake()
     {
         characterRb = GetComponent<Rigidbody2D>();
         userInput = new Controller();
     }
+
     private void OnEnable()
     {
         userInput.Enable();
@@ -47,25 +34,32 @@ public class Character : MonoBehaviour
         userInput.Player.Jump.performed += OnJumpPerformed;
         userInput.Player.Jump.canceled += OnJumpCancelled;
     }
+
     private void OnDisable()
     {
         userInput.Disable();
-        
+
         userInput.Player.Move.performed -= OnMovementPerformed;
         userInput.Player.Move.canceled -= OnMovementStopped;
 
         userInput.Player.Jump.performed -= OnJumpPerformed;
         userInput.Player.Jump.canceled -= OnJumpCancelled;
     }
-    
-    
-    // Update is called once per frame
-    protected void FixedUpdate()
-    {
-        Vector2 velocity = new Vector2(moveVector.x * speed, 0);
-        characterRb.velocity = velocity;
-        PlayerAnimations.OnSpriteChanged(velocity);
 
+    private void FixedUpdate()
+    {
+        MoveCharacter();
+        HandleJump();
+    }
+
+    private void MoveCharacter()
+    {
+        Vector2 velocity = new Vector2(moveVector.x * speed, characterRb.velocity.y);
+        characterRb.velocity = velocity;
+    }
+
+    private void HandleJump()
+    {
         if (canJump && jumped)
         {
             Debug.Log("Jumped!!");
@@ -77,24 +71,25 @@ public class Character : MonoBehaviour
     private void OnMovementPerformed(InputAction.CallbackContext value)
     {
         moveVector = value.ReadValue<Vector2>();
-        //Debug.Log(moveVector);
-        //PlayerAnimations.OnSpriteChanged(moveVector);
+        // Debug.Log(moveVector);
     }
 
     private void OnMovementStopped(InputAction.CallbackContext value)
     {
         moveVector = Vector2.zero;
-        //PlayerAnimations.OnSpriteChanged(moveVector);
     }
 
     private void OnJumpPerformed(InputAction.CallbackContext value)
     {
-        jumped = true;
-        Debug.Log(value.ToString());
+        if (canJump)
+        {
+            jumped = true;
+        }
     }
 
     private void OnJumpCancelled(InputAction.CallbackContext value)
     {
         jumped = false;
+        canJump = true;
     }
 }
