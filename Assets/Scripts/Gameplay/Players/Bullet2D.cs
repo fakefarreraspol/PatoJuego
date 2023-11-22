00e04747f5311822fd9832bb9ef9654a561beabe
aNnAm2606 <73582949+aNnAm2606@users.noreply.github.com>
2023-11-22 17:27:19 +0100

    Bullet when collides with walls and ground

--- Assets/Scripts/Gameplay/Players/Bullet2D.cs
+++ Assets/Scripts/Gameplay/Players/Bullet2D.cs
@@ -6,20 +6,20 @@ public class Bullet2D : MonoBehaviour
 {
 
     public float speed = 10f;
-    public float lifetime = 2.0f; // Set the lifetime in seconds
+   
     public Vector2 dir = Vector2.zero;
     void Update()
     {
         // Move the bullet forward
         transform.Translate(dir * speed * Time.deltaTime);
+    }
 
-        // Decrease the lifetime
-        lifetime -= Time.deltaTime;
-
-        // Check if the lifetime has expired
-        if (lifetime <= 0.0f)
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the bullet collided with an object on the "Ground" layer
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
-            // Destroy the bullet
+            // Destroy the bullet when it hits the ground
             Destroy(gameObject);
         }
     }
