d397c12c619f75b17f3ff32a6170c8bbf77bceee
Pol <79444349+fakefarreraspol@users.noreply.github.com>
2023-11-23 03:18:18 +0100

    camera

---
+++ Assets/Scripts/Gameplay/Camera2D.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class Camera2D : MonoBehaviour
+{
+
+    public Transform target;  // The target object (the sprite in this case)
+    public float smoothTime = 0.3f;  // Smoothing time for the camera movement
+    public Vector3 offset;  // Offset between the camera and the target
+
+    private Vector3 velocity = Vector3.zero;
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            // Calculate the target position with offset
+            Vector3 targetPosition = target.position + offset;
+
+            // Smoothly interpolate towards the target position
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+    }
+}
